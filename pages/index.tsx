3552130cebb15db9200ccd56439b7e939a58f4bc
James Hyatt <jameshyattdevelopment@JamessMacStudio.lan>
2022-07-25 14:34:47 -0400

    enables all aspects of Stepzen, Apollo and Supabse into the application

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Header from '../components/Header'
 // import Image from 'next/image'
 
 const Home: NextPage = () => {
@@ -12,7 +11,9 @@ const Home: NextPage = () => {
       </Head>
       
       <main>
-        <Header />
+
+
+
       </main>
       
     </div>
